07a1a0b7bfe481f928ad891174db26c80cdd1e00
David Stinnette <dastinnette@gmail.com>
2016-03-08 11:14:10 -0700

    Experimenting with method setup in bestbuy service b/c I'm having trouble manipulating search results

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -1,18 +1,26 @@
 class BestbuyService
-  attr_reader :connection, :key
+  attr_reader :connection
 
   def initialize
-    @connection = Faraday.new(url: "https://api.bestbuy.com/v1/products")
-    @key        = ENV["best_buy"]
+    @connection = Faraday.new(:url => "https://api.bestbuy.com/v1/products") do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
   end
 
   def search
-    parse(connection.get("/search", key))
+    response = connection.get do |req|        # GET http://sushi.com/search?page=2&limit=100
+      req.url '/search', :page => 2
+      req.params['limit'] = 15
+    end
+    response.body
   end
 
-  private
 
-  def parse(response)
-    JSON.parse(response.body, symbolize_name: true)
-  end
+  # private
+  #
+  # def parse(response)
+  #   JSON.parse(response.body, symbolize_name: true)
+  # end
 end
