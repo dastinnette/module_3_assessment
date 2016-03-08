6e08375d3c2519ddef21bfb717afa8e3619b8282
David Stinnette <dastinnette@gmail.com>
2016-03-08 12:01:43 -0700

    Ripped out nonsense in initialize, simplifying Faraday setup hoping to lose the JSON::ParserError:        757: unexpected token at '<h1>Not Found</h1>' error. No luck

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -2,26 +2,17 @@ class BestbuyService
   attr_reader :connection, :key
 
   def initialize
-    @connection = Faraday.new(:url => "https://api.bestbuy.com/v1/products") do |faraday|
-      faraday.request  :url_encoded
-      faraday.response :logger
-      faraday.adapter  Faraday.default_adapter
-    end
+    @connection = Faraday.new(url: "https://api.bestbuy.com/v1/products")
     @key = ENV['best_buy']
   end
 
   def search_broad
-    parse(connection.get("/search=sennheiser)?format=json&show=sku,name,customerReviewAverage,shortDescription,salePrice,image&pageSize=15&apiKey=#{key}"))
-    binding.pry
-  end
-
-  def search_specific
-    parse(connection.get("/search=#{searchterm})?format=json&show=sku,name,customerReviewAverage,shortDescription,salePrice,image&pageSize=15&apiKey=#{key}"))
+    parse(connection.get("/search=#{search_term})?format=json&show=sku,name,customerReviewAverage,shortDescription,salePrice,image&pageSize=15&apiKey=#{key}"))
   end
 
   private
 
   def parse(response)
-    JSON.parse(response.body, symbolize_name: true)
+    JSON.parse(response.body)
   end
 end
