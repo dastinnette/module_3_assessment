318fd802ef60a7f2c1f956277102a4c8e7978d32
David Stinnette <dastinnette@gmail.com>
2016-03-08 10:45:49 -0700

    Building out bestbuy service PORO

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -1,5 +1,18 @@
 class BestbuyService
+  attr_reader :connection, :key
 
+  def initialize
+    @connection = Faraday.new(url: "https://api.bestbuy.com")
+    @key        = ENV["best_buy"]
+  end
 
+  def search
+    parse(connection.get("/search", key))
+  end
 
+  private
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_name: true)
+  end
 end
